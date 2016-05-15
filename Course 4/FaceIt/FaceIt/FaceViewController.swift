995137c713a4c164a824098d80b73e4c77417366
hanief <hanief@gmail.com>
2016-05-15 21:30:02 +0700

    add course 4

---
+++ Course 4/FaceIt/FaceIt/FaceViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Hanief on 5/15/16.
+//  Copyright © 2016 Cahya Utama Technologies. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+}
+
